61184a3998c452e504b3a72c3f4449a0383dd39e
Enes Saglam <108627652+SAGLAMENES@users.noreply.github.com>
2024-03-17 09:23:14 +0300

    Script homework Donegit add myScript.swift

--- myScript.swift
+++ myScript.swift
@@ -3,20 +3,22 @@
 import Foundation
 
 
-/*var users = ["Enes","Rezzak","Sinem","Ahmet","Mehmet"]
+var users = ["Enes","Rezzak","Sinem","Ahmet","Mehmet"]
 
 if users.contains(""){
 	print("Error, empty username")
 }else{
-	for user in users {
-	print(user.capitalized)
-	print(user.uppercased())
+	users = users.sorted(by: { $0 > $1 })
+	for index in 0..<users.count {
+	users[index]=users[index].uppercased()
+	print(users[index])
 	}
+	
 }
-*/
 
-print("Enter your name:")
+
+/*print("Enter your name:")
 var name = readLine(strippingNewline: true)
-print("Hello \(name ?? "anon")")
+print("Hello \(name ?? "anon")")*/
 // Odev: usersların tüm harflerini büyütüp Z-A ya sıralı olacak şekilde yazınız
 
